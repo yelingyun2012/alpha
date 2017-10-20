e3fe366f10143e84a5ef6fe3c579b6d0caa5d4e2
haochen <2390154725@qq.com>
2017-10-20 11:20:47 +0800

    'alpha'

--- saiku/src/utils/fundebug.js
+++
@@ -1,3 +0,0 @@
-import * as fundebug from 'fundebug-javascript'
-
-fundebug.apikey = '193c68f5a0b0cb42703a7fa4224574d06bac33dd803da781289df5dffb2894cd'
--- template/src/router/_import-development.js
+++
@@ -1,2 +0,0 @@
-// module.exports = file => require(`../component/${file}.vue`).default
-module.exports = file => () => import(`../component/${file}.vue`)
