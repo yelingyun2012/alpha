14a6f0d727edb75dbd12726eb9c23bc2edc252f7
kabofai <598799526@qq.com>
2017-11-24 17:44:03 +0800

    update

--- bread/src/component/basic/pageModel.vue
+++ bread/src/component/basic/pageModel.vue
@@ -201,149 +201,112 @@ export default {
         this.$Message.error(error.match(/([^\[\]]+)(?=\])/g)[0]);
       }
     },
-    methods: {
-      confirm(modelId, index) {
-        this.$Modal.confirm({
-          content: "<p>是否确定删除该模型</p>",
-          onOk: () => {
-            this.initPageModelDelete(modelId, index);
-          }
+    async initPageModelList() {
+      try {
+        let res = await pageModelList({
+          pageIndex: this.pageIndex,
+          pageSize: this.pageSize,
+          siteId: this.siteId,
+          modelName: this.pageModelName,
+          token: getCookie("token")
         });
-      },
-      handleSearch() {
-        this.pageIndex = 1;
-        this.initPageModelList();
-      },
-      handleAdd() {
-        this.$router.push("/pageManager/add");
-      },
-      handleCut(subscript, siteId) {
-        this.activeIndex = subscript;
-        this.siteId = siteId;
-        this.pageIndex = 1;
-        this.initPageModelList();
-      },
-      handlePage(pageIndex) {
-        this.pageIndex = pageIndex;
-        this.initPageModelList();
-      },
-      async initSite() {
-        try {
-          let res = await siteNum({ typeId: 1, token: getCookie("token") });
-          this.siteList = [...this.siteList, ...res.data.data];
-        } catch (error) {
-          this.$Message.error(error.match(/([^\[\]]+)(?=\])/g)[0]);
-        }
-      },
-      async initPageModelList() {
-        try {
-          let res = await pageModelList({
-            pageIndex: this.pageIndex,
-            pageSize: this.pageSize,
-            siteId: this.siteId,
-            modelName: this.pageModelName,
-            token: getCookie("token")
-          });
-          this.pageTotal = res.data.data.recordCount;
-          this.pageModelData = res.data.data.result;
-        } catch (error) {
-          this.$Message.warning(error.match(/([^\[\]]+)(?=\])/g)[0]);
-          this.pageModelData = [];
-        }
-      },
-      async initPageModelDelete(modelId, index) {
-        try {
-          await pageModelDelete({
-            modelId: modelId,
-            token: getCookie("token")
-          });
-          this.pageModelData.splice(index, 1);
-          this.pageTotal = this.pageTotal - 1;
-          this.$Message.success("删除成功");
-        } catch (error) {
-          this.$Message.error(error.match(/([^\[\]]+)(?=\])/g)[0]);
-        }
+        this.pageTotal = res.data.data.recordCount;
+        this.pageModelData = res.data.data.result;
+      } catch (error) {
+        this.$Message.warning(error.match(/([^\[\]]+)(?=\])/g)[0]);
+        this.pageModelData = [];
       }
     },
-    created() {
-      this.initSite();
-      this.initPageModelList();
+    async initPageModelDelete(modelId, index) {
+      try {
+        await pageModelDelete({
+          modelId: modelId,
+          token: getCookie("token")
+        });
+        this.pageModelData.splice(index, 1);
+        this.pageTotal = this.pageTotal - 1;
+        this.$Message.success("删除成功");
+      } catch (error) {
+        this.$Message.error(error.match(/([^\[\]]+)(?=\])/g)[0]);
+      }
     }
+  },
+  created() {
+    this.initSite();
+    this.initPageModelList();
   }
 };
 </script>
 
 <style lang="stylus">
- // 公共函数
-  taskWrapper(top,right,bottom,left)
-    padding top right bottom left
-    background-color #fff
-  .public
-    &-name
-      margin-bottom 20px
-      taskWrapper 30px 20px 30px 20px
-      .ivu-input
-        height 35px
-      .explain
-        color #323232
-        font-size 14px
-      .typeIn
-        margin-right 30px
-        margin-left 4px
-      .ivu-btn
-        padding 6px 23px
-        &:last-child
-          margin-left 10px
-          border-color #108EE9
-          color #108EE9
-          opacity .9
-          &:hover
-            border-color #57a3f3
-            color #57a3f3
-    &-minute // 任务详细
-      taskWrapper 20px 20px 20px 20px
-    &-site
-      padding-bottom 10px
-      border-bottom 1px dashed #B7B7B7
-      margin-bottom 20px
-      span
-        display inline-block
-        padding 6px 15px
-        color #589BEE
-        font-size 14px
-        cursor pointer
-        &.task-site-active
-          border-radius 4px
-          background-color #2D8CF0
-          color #fff
-        i
-          font-style inherit
-    &-mission
-    .ivu-page
-      margin-top 20px
-      margin-right 20px
-      text-align right
-  .signInTab
-    display inline-block
-    padding 4px 10px
-    border 1px solid #A7E1C4
-    border-radius 2px
-    background #EBF8F2
-    color #646464
-    opacity .7
-  .signOutTab
-    display inline-block
-    padding 4px 10px
-    border 1px solid #FABEB9
-    border-radius 2px
-    background #FFF5F4
-    color #646464
-    opacity .7
-  .vertical-center-modal
-    display flex
-    justify-content center
-    align-items center
-    .ivu-modal
-      top 0
+taskWrapper(top,right,bottom,left)
+  padding top right bottom left
+  background-color #fff
+.public
+  &-name
+    margin-bottom 20px
+    taskWrapper 30px 20px 30px 20px
+    .ivu-input
+      height 35px
+    .explain
+      color #323232
+      font-size 14px
+    .typeIn
+      margin-right 30px
+      margin-left 4px
+    .ivu-btn
+      padding 6px 23px
+      &:last-child
+        margin-left 10px
+        border-color #108EE9
+        color #108EE9
+        opacity .9
+        &:hover
+          border-color #57a3f3
+          color #57a3f3
+  &-minute // 任务详细
+    taskWrapper 20px 20px 20px 20px
+  &-site
+    margin-bottom 20px
+    padding-bottom 10px
+    border-bottom 1px dashed #B7B7B7
+    span
+      display inline-block
+      padding 6px 15px
+      color #589BEE
+      font-size 14px
+      cursor pointer
+      &.task-site-active
+        border-radius 4px
+        background-color #2D8CF0
+        color #fff
+      i
+        font-style inherit
+  &-mission, .ivu-page
+    margin-top 20px
+    margin-right 20px
+    text-align right
+.signInTab
+  display inline-block
+  padding 4px 10px
+  border 1px solid #A7E1C4
+  border-radius 2px
+  background #EBF8F2
+  color #646464
+  opacity .7
+.signOutTab
+  display inline-block
+  padding 4px 10px
+  border 1px solid #FABEB9
+  border-radius 2px
+  background #FFF5F4
+  color #646464
+  opacity .7
+.vertical-center-modal
+  display flex
+  justify-content center
+  align-items center
+  .ivu-modal
+    top 0
 </style>
 
